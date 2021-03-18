92e111fa0c4c80570e59abecc058bd7ad4646a67
Johan van der Wijk <johan@vanderwijk.nl>
2021-03-18 13:32:16 +0100

    Translations

--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php
 
-
-
 // enqueue the child theme stylesheet
 
 Function wp_schools_enqueue_scripts() {
 wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-wp_enqueue_style( 'childstyle' );
+	wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+// Load translation files from your child theme instead of the parent theme
+function verometal_child_theme_locale() {
+	load_child_theme_textdomain( 'verometal', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'verometal_child_theme_locale' );
+
+function verometal_strings() {
+	__('coating','verometal');
+}
